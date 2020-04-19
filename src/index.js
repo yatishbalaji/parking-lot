825531eadb641224c44214bd3852d39a31ae0eab
yatishbalaji <yatishmotamarri1993@gmail.com>
2020-04-19 16:31:45 +0530

    feat(parkingLot): Create Parking Lot

--- src/index.js
+++ src/index.js
@@ -25,6 +25,17 @@ class ParkingLot {
       const inputCommands = fs.readFileSync(this.location)
         .toString()
         .split('\n').map(cmd => cmd.split(' '));
+
+      for (let [cmd, arg1, arg2 = ''] of inputCommands) {
+        switch(cmd) {
+            case 'create_parking_lot':
+                log(`Created parking lot with ${arg1} slots`)
+                this.lot = new Array(+arg1).fill(null);
+                break;
+            default:
+                log('Broken command');
+        }
+      }
   }
 }
 
